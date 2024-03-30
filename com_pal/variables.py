d2ec5d352bbcdf5d3b28b90125cd4b848581bc40
o-master-o <kasian.m.w@gmail.com>
2024-03-30 20:18:45 +0100

    make string paths in variables

--- com_pal/variables.py
+++ com_pal/variables.py
@@ -1,3 +1,2 @@
-from pathlib import Path
-
-CONFIG_DIR_PATH = Path.home() / '.config/com_pal'
+CONFIG_DIR_PATH = '/home/yoda/.config/com_pal'
+GTP_MODEL_PATH = f'{CONFIG_DIR_PATH}/gpt4all-falcon-newbpe-q4_0.gguf'
